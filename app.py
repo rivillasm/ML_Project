65d9c92ba609c941ad4afd44515f9c904fba6f17
rivillasm <rivillasm@gmail.com>
2022-06-22 23:59:01 -0600

    updated

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 
 from flask import Flask
 from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 
 def index():
-    logging.info("testing logging module")
+    try:
+        raise Exception("Wea re testing custom exception")
+    except Exception as e:
+        raise HousingException(e,sys) from e
+        logging.info(housing.error_message)
+        logging.info("testing logging module")
     return "Starting Machine Learning Project"
 
 if __name__=="__main__":
---
+++ housing/exception/__init__.py
@@ -0,0 +1,29 @@
+
+import sys
+import os
+
+class HousingException(Exception):    # getting exceptio from the father class
+
+    def __init__(self, error_message:Exception, error_detail:sys):   # gets error message and details
+        super().__init(error_message)                                     # passes the mesage to the exception class
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                       error_detail=error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception, error_detail:sys)->str:
+        """
+        error message: Exception object
+        error detail : object of sys module
+        """
+        _,_,exec_tb = error_detail.exc_info()     # exc_info() requires parameters: type,value,traceback
+                                                  # we are only interested in the traceback parameter
+        line_number = exec_tb.tb_frame.f_lineno            # gets the line number
+        file_name = exec_tb.tb_frame.f_code.co_filename    # gets the file name
+        error_message = f"Error occured in script:[{file_name} at line number [{line_number}] error message [{error_message}]]"
+        return error_message
+
+    def __str__(self):
+        return self.error_message
+
+    def __repr__(self)-> str:
+        return HousingException.__name__.str()
