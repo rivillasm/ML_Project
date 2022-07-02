98375e3855b5ac090cba5741f9dd0c8835100a8b
rivillasm <rivillasm@gmail.com>
2022-07-01 23:44:40 -0600

    config

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -2,6 +2,12 @@ from housing.entity.config_entity import DataIngestionConfig
 from housing.exception import HousingException
 import sys,os
 from housing.logger import logging
+from housing.entity.artifact_entity import DataIngestionArtifact
+import tarfile                          # to extract the data
+from six.moves import urllib            # a package that collects several modules for working with URLs
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 class DataIngestion:
@@ -11,10 +17,84 @@ class DataIngestion:
             logging.info(f"{'='*20}Data Ingestion log started.{'=*20'}")
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
-            raise HousingException(e,sys)
+            raise HousingException(e,sys) from e
 
-    def initiate_data_ingestion(selfself) -> DataIngestionArtifact:
+    def download_housing_data(self,) -> str:
         try:
-            pass
+            # extraction remote url to download dataset
+            download_url = self.data_ingestion_config.dataset_download_url   # gets the full url address
+
+            # folder location to download file
+            tgz_download_dir = self.data_ingestion_config.tgz_download_dir
+
+            if os.path.exists(tgz_download_dir):
+                os.remove(tgz_download_dir)                 # if the folder exists, gets removed
+            os.makedirs(tgz_download_dir,exist_ok=True)     # creates a directory recursively based on a given path
+
+            housing_file_name = os.path.basename(download_url)   # extracts only the name of the file from the url
+
+            tgz_file_path = os.path.join(tgz_download_dir,housing_file_name)
+
+            logging.info(f"Downloading file from: [{download_url}] into: [{tgz_file_path}]")
+            urllib.request.urlretrieve(download_url,tgz_file_path)
+            logging.info(f"File: [{tgz_file_path}] has been downloaded successfully")
+
+            return tgz_file_path
+
         except Exception as e:
-            raise HousingException(e,sys)
+            raise HousingException(e, sys) from e
+
+    def extract_tgz_file(self, tgz_file_path:str):
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir     # gets the directory name for the raw folder
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)                 # if the folder exists, gets removed
+            os.makedirs(raw_data_dir,exist_ok=True)     # creates a directory recursively based on a given path
+
+            logging.info(f"Extracting tgz file [{tgz_file_path}] into dir: [{raw_data_dir}]")
+            with tarfile.open(tgz_file_path) as housing_tgz_file_obj:
+                housing_tgz_file_obj.extractall(path=raw_data_dir)
+            logging.info(f"Extracting tgz completed")
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+
+    def split_data_as_train_test(self) -> DataIngestionArtifact:
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+            file_name = os.listdir(raw_data_dir)[0]             # gets the name of the first file in the list
+
+            housing_file_path = os.path.join(raw_data_dir, file_name)
+            housing_data_frame = pd.read_csv(housing_file_path)
+
+            # Bin values into discrete intervals.
+            housing_data_frame["income_cat"] = pd.cut(housing_data_frame["median_income"],
+                                                      bins=[0.0,1.5,3.0,4.5,6.0,np.inf],
+                                                      labels[1,2,3,4,5])
+
+            # split the data inot training and test
+            strat_train_set = None
+            strat_test_set = None
+            split = StratifiedShuffleSplit(n_splits=1, test_size=0,2, random_state=42)
+            for train_index, test_index in split.split(housing_data_frame,housing_data_frame["income_cat"]):
+                strat_train_set=housing_data_frame.iloc[train_index].drop(["income_cat"], axis=1)
+                strat_test_set = housing_data_frame.iloc[test_index].drop(["income_cat"], axis=1)
+
+            # create the files path
+            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir, file_name)
+            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir, file_name)
+
+
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+
+
+    def initiate_data_ingestion(self) -> DataIngestionArtifact:
+        try:
+            tgz_file_path = self.download_housing_data()          # gets the file location and downloads it
+            self.extract_tgz_file(tgz_file_path=tgz_file_path)
+        except Exception as e:
+            raise HousingException(e,sys) from e
--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -8,8 +8,8 @@ from housing.logger import logging
 
 class Configuration:
 
-    def __init__(self, config_file_path:str =CONFIG_FILE_PATH,
-                       current_time_stamp:str =CURRENT_TIME_STAMP) -> None:
+    def __init__(self, config_file_path:str = CONFIG_FILE_PATH,
+                       current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
         try:
             self.config_info = read_yaml_file(config_file_path)
             self.get_training_pipeline_config = self.get_training_pipeline_config()
@@ -78,7 +78,7 @@ class Configuration:
     def get_model_pusher_config(self) -> modelPusherConfig:
         pass
 
-    def get_training_pipeline_config(self)-> TrainingPipelineConfig :
+    def get_training_pipeline_config(self) -> TrainingPipelineConfig:
         try:
             training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
             artifact_dir = os.path.join(ROOT_DIR,
